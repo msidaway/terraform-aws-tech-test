74ce90ef8a40fe512408a7b2add1c75eceb01756
msidaway <sidawaym@rocketmail.com>
2020-06-17 14:40:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,42 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
+  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
   cidr_block           = var.vpc-cidr
   enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "public-subnet" {
+  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
+
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.subnet-cidr-public
   availability_zone = "${var.region}a"
 }
 
 resource "aws_route_table" "public-subnet-route-table" {
+  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
   vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_internet_gateway" "igw" {
+  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
   vpc_id = aws_vpc.vpc.id
 }
 
@@ -32,11 +53,20 @@ resource "aws_route_table_association" "public-subnet-route-table-association" {
   route_table_id = aws_route_table.public-subnet-route-table.id
 }
 
-resource "aws_key_pair" "web" {
+resource "aws_key_pair" "web" {  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
   public_key = file(pathexpand(var.public_key))
 }
 
 resource "aws_instance" "web-instance" {
+  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
   ami                         = "ami-cdbfa4ab"
   instance_type               = "t2.small"
   vpc_security_group_ids      = [aws_security_group.web-instance-security-group.id]
@@ -52,6 +82,11 @@ EOF
 }
 
 resource "aws_security_group" "web-instance-security-group" {
+  tags = {
+    Name = "MartynSidaway"
+    Owner = "Martyn Sidaway"
+    Project = "Tech Test"
+  }
   vpc_id = aws_vpc.vpc.id
 
   ingress {
